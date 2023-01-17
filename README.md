2ce41ea50bc2527534b5cd2f53780b38ed139090
Gustavo Bastian <bastian.gustavo@gmail.com>
2023-01-17 13:16:29 -0300

    retrieving user from database

--- README.md
+++ README.md
@@ -6,3 +6,10 @@ In order to run, you must have a file called ".env" with the following contents:
 DB_MONGO:mongodb+srv://xxxx:xxxx@xxxxx.xxxx.mongodb.net/users
 PORT_LOCAL = 8001
 ´´´
+
+## Installing components and running
+
+´´´ 
+npm install
+npm run dev
+´´´ 
--- controller/userController.js
+++ controller/userController.js
@@ -1,15 +1,48 @@
+const User = require('../models/User.js');
+
 exports.createUser = (req,res)=>{
-    res.send("<h1>user created</h1>")
+    try {
+        const code = req.params.id;
+        console.log(code);
+        //res.send(localUser.code)
+        res.send("<h1>user created</h1>")
+        }catch(e){
+            console.log(e);
+            res.status(500).send("There was an error getting user")
+        }
 }
 
-exports.sendUser = (req,res)=>{
-    res.send("<h1>Gustavito</h1>")
+exports.getUser = async (req,res)=>{
+    try {
+    const code = req.params.id;
+    console.log(code);
+    const localUser = await User.findOne();
+    console.log(localUser.password);
+    res.send(localUser.code)
+    }catch(e){
+        console.log(e);
+        res.status(500).send("There was an error getting user")
+    }
 }
 
-exports.alterUser = (req,res)=>{
-    res.send("<h1>Alterado Gustavito</h1>")
+exports.alterUser = (req,res)=>{    
+    try {
+        const code = req.params.id;
+        console.log(code);        
+        res.send("<h1>Alterado Gustavito</h1>");
+        }catch(e){
+            console.log(e);
+            res.status(500).send("There was an error getting user")
+        }    
 }
 
-exports.deleteUser = (req,res)=>{
-    res.send("<h1>borrando Gustavito</h1>")
+exports.deleteUser = (req,res)=>{    
+    try {
+        const code = req.params.id;
+        console.log(code);        
+        res.send("<h1>borrando Gustavito</h1>")
+        }catch(e){
+            console.log(e);
+            res.status(500).send("There was an error getting user")
+        }
 }
